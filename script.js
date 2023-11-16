e263b6ee8ef2be48584be6e2f7628ea860bc1839
geanpaulo29 <geanp.antonio@gmail.com>
2023-11-16 13:17:29 -0300

    commit all

---
+++ script.js
@@ -0,0 +1,39 @@
+function scrollToAbility(className) {
+    var targetElement = document.querySelector("." + className);
+
+    if (targetElement) {
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+}
+
+function scrollToObj(elementId) {
+    var targetElement = document.getElementById(elementId);
+
+    if (targetElement) {
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+}
+
+function scrollToAcademic(elementId) {
+    var targetElement = document.getElementById(elementId);
+
+    if (targetElement) {
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+}
+
+function scrollToProfss(elementId) {
+    var targetElement = document.getElementById(elementId);
+
+    if (targetElement) {
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+}
